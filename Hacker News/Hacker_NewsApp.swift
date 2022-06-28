6cbb064e65c9633ef7909fae0acaa9c87be562f4
vishad-tyagi <78733360+vishad-tyagi@users.noreply.github.com>
2022-06-28 08:37:11 +0530

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Vishad Tyagi on 28/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
